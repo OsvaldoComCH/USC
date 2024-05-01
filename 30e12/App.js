6aba5bbe0af9bc4b182ef300446433bd107253da
Eduardo Henrique Ribeiro da Silva <146580680+Analiovilsson@users.noreply.github.com>
2024-04-30 21:23:16 -0300

    Create App.js

---
+++ 30e12/App.js
@@ -0,0 +1,42 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import TelaLogin from './Telas/TelaLogin';
+import TelaAddUser from './Telas/TelaAddUser';
+import TelaPosts from './Telas/TelaPosts'
+import TelaMeusPosts from './Telas/TelaMeusPosts';
+import TelaAddPost from './Telas/TelaAddPost';
+
+const Stack = createStackNavigator()
+export default function App() {  
+  	return(
+    	<NavigationContainer>
+      		<Stack.Navigator initialRouteName="login">
+        		<Stack.Screen
+				options={{ headerShown: false }}
+				name="login" 
+				component={TelaLogin}
+				/>
+				<Stack.Screen 
+				name="posts" 
+				options={{ headerShown: false }}
+				component={TelaPosts}  
+				/>
+				<Stack.Screen 
+				name="meusPosts" 
+				options={{ headerShown: false }}
+				component={TelaMeusPosts}  
+				/>
+				<Stack.Screen 
+				name="addPost" 
+				options={{ headerShown: false }}
+				component={TelaAddPost}  
+				/>
+				<Stack.Screen
+				options={{ headerShown: false }}
+				name="addUser"
+				component={TelaAddUser}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
